215338f56145cecbebd2e173cab068b7f2b80a7b
EwaMadej2004 <emadej@st.swps.edu.pl>
2026-01-30 12:17:18 +0100

    30.01 poprawki validacja

--- hair_app/serializers.py
+++ hair_app/serializers.py
@@ -14,11 +14,38 @@ class HairProfileSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["user", "created_at"]
 
+    def validate_imie(self, value):
+        if not (value.isalpha() and value[0].isupper()):
+            raise serializers.ValidationError(
+                "Imię musi zawierać tylko litery i zaczynać się wielką literą."
+            )
+        return value
+
+    def validate_nazwisko(self, value):
+        if not (value.isalpha() and value[0].isupper()):
+            raise serializers.ValidationError(
+                "Nazwisko musi zawierać tylko litery i zaczynać się wielką literą."
+            )
+        return value
 
 class HairProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = HairProduct
         fields = "__all__"
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError(
+                "Nazwa produktu musi mieć co najmniej 2 znaki."
+            )
+        return value
+
+    def validate_brand(self, value):
+        if value and not value.replace(" ", "").isalpha():
+            raise serializers.ValidationError(
+                "Marka może zawierać tylko litery i spacje."
+            )
+        return value
 
 
 class HairRoutineEntrySerializer(serializers.ModelSerializer):
